f85ee643bbbd20861767991b9f0dc28a9949abec
RocioMarcos <cherryrooma@gmail.com>
2023-06-07 14:41:59 -0300

    [EDIT] templates

--- sueldosprovincia/sueldos/forms.py
+++ sueldosprovincia/sueldos/forms.py
@@ -4,9 +4,9 @@ from django.contrib.auth.forms import AuthenticationForm
 class LoginForm(AuthenticationForm):
     username = forms.CharField(
         label="Usuario",
-        widget=forms.TextInput(attrs={'class': 'form-control'})
+        widget=forms.TextInput(attrs={'class': 'form-control inputs', 'placeholder':'DNI', 'autofocus':'autofocus', 'maxlengh':'8'})
     )
     password = forms.CharField(
         label="Contraseña",
-        widget=forms.PasswordInput(attrs={'class': 'form-control'})
+        widget=forms.PasswordInput(attrs={'class': 'form-control inputs', 'placeholder':'Contraseña'})
     )
